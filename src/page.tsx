1828e7628f1f58dc99e30e5c2358ca4cb100a8b6
Phil Cockfield <phil@cockfield.net>
2020-01-24 10:29:58 +1300

    initial commit

---
+++ src/page.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+console.log('👋 Hello React from TypeScript');
+
+// Render root react element.
+const root = <div>Hello Div</div>;
+ReactDOM.render(root, document.getElementById('root'));
